8f7d7c2bae6fbd01655dfdefe2e952738ca7423a
J o s e p h  S a m i r <joseph.samir@msa.edu.eg>
2021-01-27 14:35:07 +0200

    Added a Procfile.

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.post('/upload',async (req,res)=>{
         }
     
         res.status(200).send({
-          message: "Uploaded the file successfully: " + req?.file?.originalname,
+          message: "Uploaded the file successfully: " + req.file.originalname,
         });
       } catch (err) {
         if (err.code == "LIMIT_FILE_SIZE") {
@@ -49,7 +49,7 @@ app.post('/upload',async (req,res)=>{
             });
           }
         res.status(500).send({
-          message: `Could not upload the file: ${req?.file?.originalname}. ${err}`,
+          message: `Could not upload the file: ${req.file.originalname}. ${err}`,
         });
       }
 })
