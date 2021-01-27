63bfdfe1293f13f363c31324223e2f328c9f5e24
J o s e p h  S a m i r <joseph.samir@msa.edu.eg>
2021-01-27 13:26:17 +0200

    Added a Procfile.

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express()
+const port = process.env.PORT || 8000;
+
+app.get('/', (req,res)=>{
+    res.send("<h1>hello hello</h1>")
+})
+
+app.listen(port, ()=>{
+    console.log("working")
+})
